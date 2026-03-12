ca4ea101dcba35673a5255ca70b4aec40fce1177
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-03-12 14:03:13 +0530

    -

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // const API = 'http//localhost:5000'
 
 const API = axios.create({ 
-baseURL:'http://localhost:5000/api' }) // https://shakti-toys-backend.onrender.com/api
+baseURL:'https://shakti-toys-backend.onrender.com/api' }) //   http://localhost:5000/api
 
 API.interceptors.request.use(config => {
   const user = JSON.parse(localStorage.getItem('shakti_user') || 'null')
